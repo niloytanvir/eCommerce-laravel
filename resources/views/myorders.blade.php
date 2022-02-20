a478d099f03141a5bbab0525fe65540f0264a63b
Tanvir Ahmed <2017-1-60-131@std.ewubd.edu>
2022-02-20 18:56:01 +0600

    Order page completed

--- resources/views/myorders.blade.php
+++ resources/views/myorders.blade.php
@@ -3,9 +3,9 @@
 <div class="custom-product">
     <div class="col-sm-10">
         <div class="trending-wrapper">
-            <h4>Cart List</h4>
+            <h4>My Orders</h4>
             <a class="btn btn-success" href="ordernow">Order Now</a> <br><br><br>
-            @foreach($products as $item)
+            @foreach($orders as $item)
             <div class="row searched-item cart-list-devider">
                 <div class="col-sm-3">
                     <a href="detail/{{$item->id}}">
@@ -14,23 +14,20 @@
                 </div>
                 <div class="col-sm-4">
                         <div class="">
-                            <h2>{{$item->name}}</h2>
-                            <h5>{{$item->description}}</h5>
+                            <h2>Name : {{$item->name}}</h2>
+                            <h5>Delivary Status : {{$item->status}}</h5>
+                            <h5>Address : {{$item->address}}</h5>
+                            <h5>Payment Status : {{$item->payment_status}}</h5>
+                            <h5>Payment Method : {{$item->payment_method}}</h5>
 
                         </div>
                     </a>
                 </div>
-                <div class="col-sm-3">
-                    <a href="/removecart/{{$item->cart_id}}"  class="btn btn-warning">Remove from Cart</a>
-                   
-                </div>
-
 
             </div>
             @endforeach
 
         </div>
-        <a class="btn btn-success" href="ordernow">Order Now</a> <br><br><br>
     </div>
 </div>
 @endsection
