0245e16164f5bb2e2e016eef6be22ad4ddf6e1fe
Tanvir Ahmed <2017-1-60-131@std.ewubd.edu>
2022-02-20 22:46:08 +0600

    little fix in home page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -24,7 +24,7 @@ $total= ProductController::cartItem();
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home </a></li>
+                <li class="active"><a href="/">Home </a></li>
                 <li class=""><a href="/myorders">Orders </a></li>
 
             </ul>
